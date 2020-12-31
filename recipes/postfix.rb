3a6980d7d7611add8631770e0c956b8d6282c519
Ernie Hershey <github@ernie.org>
2020-12-31 14:38:04 -0500

    fix node attribute specification

--- recipes/postfix.rb
+++ recipes/postfix.rb
@@ -1,11 +1,11 @@
 chef_gem 'chef-vault'
 
-default['postfix']['aliases']['ernie'] = "ernie,ehershey+#{node['hostname']}@gmail.com"
+node.default['postfix']['aliases']['ernie'] = "ernie,ehershey+#{node['hostname']}@gmail.com"
 
 vault = ChefVault::Item.load(cookbook_name, recipe_name)
 
 vault['relayhosts'].each do |relayhost,value|
-  default['postfix']['sasl'][relayhost] = value
-  default['postfix']['main']['relayhost'] = relayhost
+  node.default['postfix']['sasl'][relayhost] = value
+  node.default['postfix']['main']['relayhost'] = relayhost
 end
 
