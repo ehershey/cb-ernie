2a681c164890f4527924c8ec10b37ac7f178149f
Ernie Hershey <github@ernie.org>
2021-05-25 17:20:55 -0400

    misc

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 name 'ernie'
-version '0.3.7'
+version '0.3.9'
 
 depends 'logrotate'
 depends 'postfix'
+depends 'homebrew'
---
+++ recipes/server.rb
@@ -0,0 +1,4 @@
+
+if platform_family?('mac_os_x') then
+  include_recipe "ernie::macos"
+end
