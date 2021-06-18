5a92df7e3cc8a3cbc831e7dddb86b8f13dd1f387
Ernie Hershey <github@ernie.org>
2021-06-18 09:29:45 -0400

    Remove extra mac stuff

--- recipes/workstation.rb
+++ recipes/workstation.rb
@@ -1,16 +1,3 @@
-#package "geolessel/homebrew-repo/trello-cli" do
-  #only_if { platform_family?("mac_os_x") }
-#end
-#
-#include_recipe "nodejs"
-#npm_package 'trello-cli' do
-  #url "git@github.com:mheap/trello-cli.git"
-#end
-#
-macos_desktop_screensaver do
-  only_if { platform_family?("mac_os_x") }
-end
-
 # for gnucash
 cpan_module 'Finance::Quote'
 
