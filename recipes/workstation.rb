cc70009fd51f4ef5dd78ec3627b6d0e03fa3a024
Ernie Hershey <github@ernie.org>
2021-07-13 15:38:51 -0400

    clone vundle

--- recipes/workstation.rb
+++ recipes/workstation.rb
@@ -5,3 +5,20 @@ cpan_module 'Finance::Quote'
 if platform_family?('mac_os_x') then
   include_recipe "ernie::macos"
 end
+
+user = node['ernie']['user']
+home = File.expand_path("~#{user}")
+
+vimdir = "#{home}/.vim"
+
+bundledir = "#{home}/.vim/bundle/Vundle.vim"
+
+directory bundledir do
+  owner user
+end
+
+
+execute "git clone VundleVim" do
+    command "git clone https://github.com/VundleVim/Vundle.vim.git #{bundledir}"
+    not_if { ::File.exist? "#{bundledir}/.git" }
+end
