7bc17fc231d6cd4798e720d4f872ecec766267a1
Ernie Hershey <github@ernie.org>
2021-08-24 19:21:41 -0400

    Don't use platform or platform family

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,7 @@ if node['ernie']['packages'] && node['ernie']['packages']['all'] then
 end
 
 user = node['ernie']['user']
-gopath = node['ernie']['gopath'][node['platform']]
+gopath = node['ernie']['gopath'][node['os']]
 
 directory gopath do
   owner user
