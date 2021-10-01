1005c3520755dac9b9f962810672b0ff20d1bfe2
Ernie Hershey <github@ernie.org>
2021-10-01 14:23:25 -0400

    lint fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,7 @@ if node['ernie']['packages'] && node['ernie']['packages'][node['platform_family'
   end
 end
 
-if node['ernie']['packages'] && node['ernie']['packages']['all'] then
+if node['ernie']['packages'] && node['ernie']['packages']['all']
   node['ernie']['packages']['all'].each do |package|
     package package
   end
@@ -26,23 +26,23 @@ directory gopath do
   recursive true
 end
 
-if node['ernie']['go_packages'] then
+if node['ernie']['go_packages']
   node['ernie']['go_packages'].each do |package|
     execute "go-get-#{package}" do
       command "go get #{package}"
       user user
       environment({
         GOPATH: "#{gopath}",
-        GOCACHE: "/tmp",
+        GOCACHE: '/tmp',
       })
-        ## Use the package-installed Go as the bootstrap version b/c Go is built with Go
-        #GOROOT_BOOTSTRAP: "#{new_resource.install_dir}/go-#{new_resource.version}",
-        #GOROOT: "#{new_resource.install_dir}/go",
-        #GOBIN: "#{new_resource.install_dir}/go/bin",
-      #})
-      #not_if "test -x #{::File.join(new_resource.install_dir, 'go', 'bin', 'go')}  && #{::File.join(new_resource.install_dir, 'go', 'bin', 'go')}  version | grep #{new_resource.source_version}"
-      #not_if { ::File.exist? "/opt/chef-workstation/bin/uninstall_chef_workstation" }
-      end
+      # # Use the package-installed Go as the bootstrap version b/c Go is built with Go
+      # GOROOT_BOOTSTRAP: "#{new_resource.install_dir}/go-#{new_resource.version}",
+      # GOROOT: "#{new_resource.install_dir}/go",
+      # GOBIN: "#{new_resource.install_dir}/go/bin",
+      # })
+      # not_if "test -x #{::File.join(new_resource.install_dir, 'go', 'bin', 'go')}  && #{::File.join(new_resource.install_dir, 'go', 'bin', 'go')}  version | grep #{new_resource.source_version}"
+      # not_if { ::File.exist? "/opt/chef-workstation/bin/uninstall_chef_workstation" }
+    end
   end
 end
 
