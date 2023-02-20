756ca5f991fc4c63b7db9e94877c3a361b4154a5
Ernie Hershey <github@ernie.org>
2023-02-20 14:48:01 -0500

    use attrbiute for listen interface

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -1,3 +1,11 @@
+  apache_exporter_port = "9117"
+	interface_name = node['prometheus_exporters']['listen_interface']
+  interface = node['network']['interfaces'][interface_name]
+  listen_ip = interface['addresses'].find do |_address, data|
+    data['family'] == 'inet'
+  end.first
+
 apache_exporter 'main' do
+	telemetry_address "#{listen_ip}:#{apache_exporter_port}"
 	user "nobody"
 end
