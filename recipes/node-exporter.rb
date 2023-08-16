25e2c7e71e28cbe934590e4a9a4729427b4ac8f6
Ernie Hershey <github@ernie.org>
2023-08-16 10:03:22 -0400

    safe_local_addresses for node_exporter

--- recipes/node-exporter.rb
+++ recipes/node-exporter.rb
@@ -1,13 +1,18 @@
-if node['os'] == 'linux'
+def get_safe_local_addresses()
+  return ['127.0.0.1']
+  end
+  if node['os'] == 'linux'
     include_recipe 'prometheus_exporters::node'
 elsif node['os'] == 'darwin'
   package 'node_exporter'
   node_exporter_port = '9100'
-	interface_name = node['prometheus_exporters']['listen_interface']
-  interface = node['network']['interfaces'][interface_name]
-  listen_ip = interface['addresses'].find do |_address, data|
-    data['family'] == 'inet'
-  end.first
+  safe_local_addresses = get_safe_local_addresses()
+
+  web_listen_addresses = ""
+  safe_local_addresses.each do |listen_ip|
+     Chef::Log.error("listen_ip: #{listen_ip}")
+    web_listen_addresses = "#{web_listen_addresses} --web-listen-address=#{listen_ip}:#{node_exporter_port}"
+  end
 
   username = node['ernie']['user']
   Chef::Log.error("username: #{username}")
@@ -15,9 +20,13 @@ elsif node['os'] == 'darwin'
   # doesn't seem to work 2023-04-07 - empty
   # listen_ip=`sudo -u #{username} /Applications/Tailscale.app/Contents/MacOS/Tailscale ip -4`
 
-  Chef::Log.error("listen_ip: #{listen_ip}")
+  # Chef::Log.error("listen_ip: #{listen_ip}")
+  #
+  #
 
-	web_listen_address = "#{listen_ip}:#{node_exporter_port}"
+  Chef::Log.error("safe_local_addresses: #{safe_local_addresses}")
+
+  Chef::Log.error("web_listen_addresses: #{web_listen_addresses}")
 
   uid = Etc.getpwnam(username).uid
   Chef::Log.error("uid: #{uid}")
@@ -39,18 +48,12 @@ elsif node['os'] == 'darwin'
   end
 
   file "#{Homebrew::install_path}/etc/node_exporter.args" do
-    content "--web.listen-address=#{web_listen_address}"
+    content "#{web_listen_addresses}"
     notifies :restart, "service[#{service_name}]"
   end
   service service_name do
     action [:enable, :start]
     plist "/Users/#{username}/Library/LaunchAgents/homebrew.mxcl.node_exporter.plist"
-    only_if { listen_ip != '' }
-  end
-  service service_name do
-    action [:disable]
-    plist "/Users/#{username}/Library/LaunchAgents/homebrew.mxcl.node_exporter.plist"
-    only_if { listen_ip == '' }
   end
 else
   Chef::Log.error("No node exporter install method for os: #{node['os']}")
